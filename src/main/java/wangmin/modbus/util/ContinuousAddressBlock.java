29de80392d485b7bd0f98c94eff117252e800de9
wm3418925 <591593903@qq.com>
2017-01-13 00:00:11 +0800

    开始位置使用二分查找

--- src/main/java/wangmin/modbus/util/ContinuousAddressBlock.java
+++ src/main/java/wangmin/modbus/util/ContinuousAddressBlock.java
@@ -2,14 +2,44 @@ package wangmin.modbus.util;
 
 import com.google.common.collect.Lists;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 /**
- * Created by wm on 2017/1/3.
+ * Created by wm on 2016/12/30.
  * 将 不连续的数据块 拼接 为 连续的
  */
 public class ContinuousAddressBlock {
-    public static class AddressBlock {
+    // 二分查找比key小的最大值
+    private static <T> int indexedBinarySearchLower(List<? extends Comparable<? super T>> list, T key) {
+        int low = 0;
+        int high = list.size()-1;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            Comparable<? super T> midVal = list.get(mid);
+            int cmp = midVal.compareTo(key);
+
+            if (cmp < 0) {
+                low = mid + 1;
+
+                if (low > high)
+                    return mid;
+            } else if (cmp > 0) {
+                high = mid - 1;
+
+                if (low > high)
+                    return mid-1;
+            } else {
+                return mid-1; // key found
+            }
+        }
+        return -(low + 1);  // key not found
+    }
+
+    public static class AddressBlock implements Comparable<AddressBlock> {
         public int home;
         public int end;
 
@@ -18,6 +48,16 @@ public class ContinuousAddressBlock {
             this.home = home;
             this.end = end;
         }
+
+        @Override
+        public String toString() {
+            return "home="+home + ", end="+end;
+        }
+
+        @Override
+        public int compareTo(AddressBlock o) {
+            return this.home - o.home;
+        }
     }
 
     private final List<AddressBlock> l = Lists.newArrayList();
@@ -25,8 +65,12 @@ public class ContinuousAddressBlock {
     public void addBlock(int home, int end) {
         assert(0 <= home && home < end);
 
+        int lower = indexedBinarySearchLower(l, new AddressBlock(home,end));
+        if (-1 == lower)
+            lower = 0;
+
         int size = l.size();
-        for (int i=0; i<size; ++i) {
+        for (int i=lower; i<size; ++i) {
             AddressBlock ab = l.get(i);
             boolean mergeRest = false;
             if (ab.home <= home && end <= ab.end) {
@@ -77,8 +121,12 @@ public class ContinuousAddressBlock {
      * @return 返回正数代表找到, -1代表未找到
      * */
     public int getBlockIndexByStartAddress(int address) {
+        int lower = indexedBinarySearchLower(l, new AddressBlock(address, address));
+        if (-1 == lower)
+            return -1;
+
         int size = l.size();
-        for (int i=0; i<size; ++i) {
+        for (int i=lower; i<size; ++i) {
             AddressBlock ab = l.get(i);
             if (ab.home <= address && address < ab.end)
                 return i;
@@ -93,6 +141,7 @@ public class ContinuousAddressBlock {
     public int getBlockCount() {return l.size();}
 
 
+
     public static void main(String[] argv) {
         ContinuousAddressBlock cab = new ContinuousAddressBlock();
         cab.addBlock(0, 4); for (AddressBlock ab : cab.getBlocks()) {System.out.print(ab.home + "-" + ab.end + " ");} System.out.println("");
@@ -105,6 +154,6 @@ public class ContinuousAddressBlock {
         cab.addBlock(0, 8); for (AddressBlock ab : cab.getBlocks()) {System.out.print(ab.home + "-" + ab.end + " ");} System.out.println("");
 
         // 测试过 (0,7~35) (4,7~35) (8,9~35) (12,13~35) (16,17~35) (18,19~35)
-        cab.addBlock(18, 35); for (AddressBlock ab : cab.getBlocks()) {System.out.print(ab.home + "-" + ab.end + " ");} System.out.println("");
+        cab.addBlock(12, 15); for (AddressBlock ab : cab.getBlocks()) {System.out.print(ab.home + "-" + ab.end + " ");} System.out.println("");
     }
 }
